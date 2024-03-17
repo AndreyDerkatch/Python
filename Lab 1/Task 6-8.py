eecd2578377cc4ff5d1117a3c86397ac7aeb4785
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 13:56:38 +0300

    Update Task 6-8.py

--- Lab 1/Task 6-8.py
+++ Lab 1/Task 6-8.py
@@ -17,3 +17,26 @@ def FindLongestSubsring(String):
 String = input("Введите строку: ")
 LongestSubsrting = FindLongestSubsring(String)
 print("Наибольшее количество идущих подряд символов кириллицы:", LongestSubsrting)
+
+#Задача 7
+#Дана строка. Необходимо найти минимальное из имеющихся в ней натуральных чисел.
+import re
+
+def FindMinNumber(String):
+    # Паттерн для поиска всех натуральных чисел в строке
+    pattern = r'\b\d+\b'
+
+    # Найти все натуральные числа в строке
+    Numbers = re.findall(pattern, String)
+
+    # Преобразовать числа из строкового формата в целочисленный и найти минимальное
+    MinNumber = min(map(int, Numbers), default=None)
+
+    return MinNumber
+
+String = input("Введите строку: ")
+MinNumber = FindMinNumber(String)
+if MinNumber is not None:
+    print("Минимальное натуральное число в строке:", MinNumber)
+else:
+    print("В строке нет натуральных чисел.")
