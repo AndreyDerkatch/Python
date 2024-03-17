8a5e355b42e1fc9284664a7e77a97a12b8b39672
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 13:10:01 +0300

    Update Task 3.py

--- Lab 1/Task 3.py
+++ Lab 1/Task 3.py
@@ -1,3 +1,14 @@
 #Вариант 5
 #Задача 7
 #Дана строка, состоящая из символов латиницы. Образуют ли прописные символы этой строки палиндром?
+def IsItPalindrome(string):
+    # Создаем список из прописных букв строки
+    letters = [letter for letter in string if letter.isalpha() and letter.isupper()]
+    # Проверяем список на симметрию
+    return letters == letters[::-1]
+
+string = input("Введите строку из символов латиницы: ")
+if IsItPalindrome(string):
+    print("Прописные символы образуют палиндром.")
+else:
+    print("Прописные символы не образуют палиндром.")
