eced32a8c36dcfe03aef37abed1bc1a406dd0917
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 13:48:40 +0300

    Update Task 6-8.py

--- Lab 1/Task 6-8.py
+++ Lab 1/Task 6-8.py
@@ -1,1 +1,19 @@
+#Вариант 5
 
+#Задача 5
+#Дана строка. Необходимо найти наибольшее количество идущих подряд символов кириллицы.
+import re
+
+def FindLongestSubsring(String):
+    # Паттерн для поиска подстрок кириллицы
+    pattern = r'[а-яА-Я]+'
+    # Найти все подстроки кириллицы в строке
+    substring = re.findall(pattern, String)
+    # Найти подстроку с максимальной длиной
+    LongestSubsrting = max(substring, key=len, default="")
+
+    return LongestSubsrting
+
+String = input("Введите строку: ")
+LongestSubsrting = FindLongestSubsring(String)
+print("Наибольшее количество идущих подряд символов кириллицы:", LongestSubsrting)
