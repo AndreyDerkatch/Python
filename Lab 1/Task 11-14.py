d610233265fbd98f4491e672df97b73ed2eaa25d
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 14:42:00 +0300

    Update Task 11-14.py

--- Lab 1/Task 11-14.py
+++ Lab 1/Task 11-14.py
@@ -22,3 +22,35 @@ sortedList = sorted(stringsList, key=AverageAsciiWeight)
 print("Строки, упорядоченные по среднему весу ASCII:")
 for string in sortedList:
     print(string)
+
+
+#Задача 6
+#Отсортировать строки в порядке увеличения медианного значения выборки строк (прошлое медианное значение удаляется из выборки и производится поиск нового медианного значения).
+def CalculateMedian(List):
+    SortedList = sorted(List)
+    n = len(SortedList)
+    if n % 2 == 0:
+        return (SortedList[n//2 - 1] + SortedList[n//2]) / 2
+    else:
+        return SortedList[n//2]
+
+def SortByMedian(Strings):
+    MedianValues = [CalculateMedian([ord(char) for char in String]) for String in Strings]
+    SortedStrings = [String for i, String in sorted(zip(MedianValues, Strings))]
+    return SortedStrings
+
+NumOfStrings = int(input("Введите количество строк: "))
+Strings = []
+
+# Считываем строки с клавиатуры и добавляем их в список
+for i in range(NumOfStrings):
+    String = input("Введите строку: ")
+    Strings.append(String)
+
+# Сортируем строки по медианному значению ASCII-кода символов
+SortedStrings = SortByMedian(Strings)
+
+# Выводим упорядоченный список строк
+print("Отсортированные строки по медианному значению выборки:")
+for String in SortedStrings:
+    print(String)
