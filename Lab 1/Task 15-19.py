e6402e7836e56779400dbbb4791aa6e89ed2093c
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 16:00:57 +0300

    Update Task 15-19.py

--- Lab 1/Task 15-19.py
+++ Lab 1/Task 15-19.py
@@ -1,1 +1,13 @@
+#Вариант 5, Задачи 17, 29, 41, 53
+def SwapMinMax(array):
+    # Функция для замены местами минимального и максимального элементов массива
+    if not array:
+        return array
 
+    minIndex = array.index(min(array))
+    maxIndex = array.index(max(array))
+
+    # Меняем местами минимальный и максимальный элементы массива
+    array[minIndex], array[maxIndex] = array[maxIndex], array[minIndex]
+
+    return array
