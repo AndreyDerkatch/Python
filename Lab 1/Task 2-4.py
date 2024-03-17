3d9f70972de9aab44306fb41910d393d453295b5
AndreyDerkatch <112868448+AndreyDerkatch@users.noreply.github.com>
2024-03-17 13:10:52 +0300

    Update Task 2-4.py

--- Lab 1/Task 2-4.py
+++ Lab 1/Task 2-4.py
@@ -1,4 +1,5 @@
 #Вариант 5
+
 #Задача 5
 #Дана строка. Перемешать все символы строки в случайном порядке
 import random
@@ -10,3 +11,17 @@ random.shuffle(letters)
 shuffled_word = ''.join(letters)
 
 print(shuffled_word)
+
+#Задача 7
+#Дана строка, состоящая из символов латиницы. Образуют ли прописные символы этой строки палиндром?
+def IsItPalindrome(string):
+    # Создаем список из прописных букв строки
+    letters = [letter for letter in string if letter.isalpha() and letter.isupper()]
+    # Проверяем список на симметрию
+    return letters == letters[::-1]
+
+string = input("Введите строку из символов латиницы: ")
+if IsItPalindrome(string):
+    print("Прописные символы образуют палиндром.")
+else:
+    print("Прописные символы не образуют палиндром.")
